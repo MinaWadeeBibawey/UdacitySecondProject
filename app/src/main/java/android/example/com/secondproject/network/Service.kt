bc9a723f021ddf9d709967b16fec506330abee8f
Mina Wadee <mina.wadae@innovitics.com>
2023-01-09 23:15:20 +0200

    Add Suspend

--- app/src/main/java/android/example/com/secondproject/network/Service.kt
+++ app/src/main/java/android/example/com/secondproject/network/Service.kt
@@ -1,29 +1,26 @@
 package android.example.com.secondproject.network
 
 import android.example.com.secondproject.Constants
-import android.example.com.secondproject.models.ImageOfTheDayModel
-import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import kotlinx.coroutines.Deferred
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+
 enum class AsteroidLisFilter(val value: String) { TODAY(""), NEXT_SEVEN_DAYS(""), SHOW_ALL("") }
 
 interface AsteroidListService {
     @GET("devbytes.json")
     fun getAsteroidList(
-        @Query("start_date") startDate: String ="",
+        @Query("start_date") startDate: String = "",
         @Query("end_date") EndDate: String = "",
         @Query("api_key") API_KEY: String
     ): String
 
     @GET("planetary/apod")
-    fun getImageOfTheDayAsync(@Query("api_key") API_KEY: String): ImageOfTheDay
+    suspend fun getImageOfTheDayAsync(@Query("api_key") API_KEY: String): ImageOfTheDay
 }
 
 private val moshi = Moshi.Builder()
